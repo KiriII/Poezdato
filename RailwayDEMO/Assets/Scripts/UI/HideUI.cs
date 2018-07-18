07aad2ae6545d4937dc5b1a39bfb5b0313a7d924
KiriII <Kulagin1999@list.ru>
2018-07-18 22:58:08 +0300

    asa

---
+++ RailwayDEMO/Assets/Scripts/UI/HideUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideUI : MonoBehaviour {
+
+    private CreatingSystem creatingSystem;
+    public Text[] text;
+    public bool forCreating;
+
+	// Use this for initialization
+	void Start () {
+        creatingSystem = FindObjectOfType<CreatingSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.gameObject.GetComponent<Button>()) this.gameObject.GetComponent<Button>().interactable = creatingSystem.lokomotiwExist;
+        this.gameObject.GetComponent<Image>().enabled = creatingSystem.cameraMove.createPoezd == forCreating;
+        for (int i = 0; i < text.Length; i++)
+        {
+            text[i].enabled = creatingSystem.cameraMove.createPoezd == forCreating;
+        }
+    }
+}
