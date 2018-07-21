9e431d4727166a9081f4e63ed38f35ec4003e6a9
KiriII <Kulagin1999@list.ru>
2018-07-21 23:47:13 +0300

    perenos
    perenos iz staroy sceni, relsov netu suka, napravlyat' poezda nelzya, pizdec, unity kashnulos' SUUUKAAAA

--- RailwayDEMO/Assets/Scripts/UI/StartButton.cs
+++ RailwayDEMO/Assets/Scripts/UI/StartButton.cs
@@ -6,20 +6,34 @@ public class StartButton : MonoBehaviour {
 
 	private Animator anim;
 
-	private void Start() 
+    private CreatingSystem Cs;
+    public GameObject startPoint;
+    public int number;
+
+
+    private void Start() 
 	{
 		anim = GetComponent<Animator>();	
 		anim.SetInteger("direction", -1);
 		
 		Time.timeScale = 0f;
 		EventHandler.TimeScaleChanged(Time.timeScale);
-	}	
+
+        Cs = FindObjectOfType<CreatingSystem>();
+    }	
 
 	public void Begin()
 	{
 		anim.SetInteger("direction", 1);
 		Time.timeScale = 1;
-		EventHandler.TimeScaleChanged(Time.timeScale);
-		EventHandler.StartEvent();
-	}
+		
+
+        if (!Cs.isCreating)
+        {
+            Cs.StartCreating(startPoint, number);
+
+        }
+        EventHandler.TimeScaleChanged(Time.timeScale);
+        EventHandler.StartEvent();
+    }
 }
